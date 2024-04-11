4247351f608384abb348241e7ba5ab84cc4586e0
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-04-11 23:19:04 +0900

    refactor: 메시지와 채팅 타입이 대소문자 구분없이 매핑되도록 수정

--- backend/ddang/src/main/java/com/ddang/ddang/websocket/handler/dto/ChatMessageType.java
+++ backend/ddang/src/main/java/com/ddang/ddang/websocket/handler/dto/ChatMessageType.java
@@ -1,23 +1,18 @@
 package com.ddang.ddang.websocket.handler.dto;
 
+import com.ddang.ddang.websocket.handler.exception.UnsupportedChattingTypeException;
+
 import java.util.Arrays;
 
 public enum ChatMessageType {
 
-    MESSAGE("message"),
-    PING("ping"),
-    ;
-
-    private final String value;
-
-    ChatMessageType(final String value) {
-        this.value = value;
-    }
+    MESSAGE,
+    PING;
 
     public static ChatMessageType findMessageType(final String value) {
         return Arrays.stream(ChatMessageType.values())
-                .filter(chattingType -> chattingType.value.equals(value))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 채팅 타입입니다."));
+                     .filter(chattingType -> chattingType.name().equalsIgnoreCase(value))
+                     .findFirst()
+                     .orElseThrow(() -> new UnsupportedChattingTypeException("잘못된 채팅 타입입니다."));
     }
 }
--- backend/ddang/src/main/java/com/ddang/ddang/websocket/handler/dto/TextMessageType.java
+++ backend/ddang/src/main/java/com/ddang/ddang/websocket/handler/dto/TextMessageType.java
@@ -1,7 +1,20 @@
 package com.ddang.ddang.websocket.handler.dto;
 
+import com.ddang.ddang.websocket.handler.exception.UnsupportedTextMessageTypeException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+
 public enum TextMessageType {
 
     CHATTINGS,
-    BIDS
+    BIDS;
+
+    @JsonCreator
+    public static TextMessageType fromString(String value) {
+        return Arrays.stream(TextMessageType.values())
+                     .filter(messageType -> messageType.name().equalsIgnoreCase(value))
+                     .findFirst()
+                     .orElseThrow(() -> new UnsupportedTextMessageTypeException("잘못된 메시지 타입입니다."));
+    }
 }
